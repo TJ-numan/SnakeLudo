2b3d63d82888007074c927e91f64c905cea827c7
Abdullah Al Numan <abdullahalnuman235@gmail.com>
2019-12-08 00:45:51 -0800

    Initial Commit

---
+++ SnakeLudo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnakeLudo
+//
+//  Created by Abdullah Al Numan(AAN) on 12/8/19.
+//  Copyright © 2019 Abdullah Al Numan(AAN). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
